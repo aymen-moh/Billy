ac7c9797a959e11cdd06c1d6dcf5f67b6a9a2654
aymen-moh <aymen.mohammed.abdl@gmail.com>
2026-09-24 17:26:28 +0300

    Made My first ever graphical program! i am so proud of this! next goal: load png instead for bmp.

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,8 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_image.h>
 #include <string>
+#include <iostream>
+
 
 constexpr int WIDTH = 600;
 constexpr int HEIGHT = 400;
@@ -9,7 +11,7 @@ SDL_Window* gWindow = nullptr;
 SDL_Surface* gScreenSurface = nullptr;
 SDL_Surface* gHelloWorld = nullptr;
 
-bool init(); {
+bool init() {
     bool success = true;
 
 
@@ -36,11 +38,64 @@ bool init(); {
 
 
 
-bool loadMedia();
-void close();
+bool loadMedia(){
+    bool success = true;
+
+    std::string imagePath{ "../Assets/images/testforsdl.bmp" };
+    gHelloWorld = SDL_LoadBMP(imagePath.c_str());
+    if(gHelloWorld == nullptr){
+        SDL_Log("window couldn't be created. %s", SDL_GetError());
+        success = false;
+
+    }
+    return success;
+}
 
 
 
-int main(int argc, char* argv[]) {
+void close(){
+    SDL_DestroySurface(gHelloWorld);
+    gHelloWorld = nullptr;
+
+    SDL_DestroyWindow(gWindow);
+    gWindow = nullptr;
+    gScreenSurface = nullptr;
+    
+    SDL_Quit();
+}
+
+
+
+int main(int argc, char* args[]) {
+    int exitcode = 0;
+    bool quit = false;
+    if(init() == false){ // i know that i can use !init() but it is more readable this way
+        exitcode = 1;
+        bool quit = false;
+    }
+
+    else{
+        if(loadMedia() == false){
+            exitcode = 2;
+            quit = true;
+        }
+        
+
+    }
+    SDL_Event e;
+    SDL_zero(e);
+    while(quit == false){
+        while(SDL_PollEvent(&e)){
+            if (e.type == SDL_EVENT_QUIT){
+                quit = true;
+            }
+        }
+        SDL_FillSurfaceRect(gScreenSurface, nullptr, SDL_MapSurfaceRGB(gScreenSurface, 0xFF, 0xFF, 0xFF));
+        SDL_BlitSurface(gHelloWorld, nullptr, gScreenSurface, nullptr);
+        SDL_UpdateWindowSurface(gWindow); 
+    }
+    close();
+
+    std::cout << "Program Exited With ExitCode: " << exitcode << '\n';
     return 0;
 }
